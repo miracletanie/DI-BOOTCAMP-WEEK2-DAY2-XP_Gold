31df49914635b4b8e668afae718b8610f7aac57b
miracletanie <michelledian23@gmail.com>
2023-01-10 18:48:53 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+
+
+  let sentence = ["my","favorite","color","is","blue"];
+  let secondSentence = sentence.join();
+  
+  console.log(secondSentence);
+ 
+
+  let firstVariable = "chritianne ";
+  let secondVariable = "yobouet";
+  
+
+  console.log(firstVariable.slice(0,2));
+  console.log(secondVariable.slice(0,2));
+  
+
+  let thirdVarible = firstVariable.concat(secondVariable)
+  
+
+  console.log(thirdVarible);
+  
+ 
+  
+    
+  let num1 = prompt("Enter your first number");
+  console.log(typeof(num1));
+  let num2 = prompt("Enter your second number");
+  sum = parseInt(num1) + parseInt(num2);
+  alert(sum);
+  
+  
+  
+  sub = parseInt(num1) - parseInt(num2);
+  alert(sub);
+
+  multi = parseInt(num1) * parseInt(num2);
+  alert(multi);
+
+  div = parseInt(num1) / parseInt(num2);
+  alert(div);
+  
